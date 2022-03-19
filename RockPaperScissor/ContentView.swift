9047ceaac5b51fab6afccb40995baae34a967b29
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-19 17:13:19 -0300

    :sparkles: Feat: create basic logical structure for the game

--- RockPaperScissor/ContentView.swift
+++ RockPaperScissor/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Property Wrappers
+    @State private var gameSelectedOption: GameOptions = .rock
+    @State private var willUserWinOrLose: Bool = Bool.random()
+    
+    // MARK: - Properties
+    private let gameOptions: [GameOptions] = GameOptions.allCases
+    
+    // MARK: - UI Components
     var body: some View {
         Text("Hello, world!")
             .padding()
---
+++ RockPaperScissor/Enumerators/GameOptions.swift
@@ -0,0 +1,23 @@
+//
+//  GameOptions.swift
+//  RockPaperScissor
+//
+//  Created by Gabriel Pereira on 19/03/22.
+//
+
+enum GameOptions: String, CaseIterable {
+    case rock = "Rock"
+    case paper = "Paper"
+    case scissor = "Scissor"
+    
+    var emoji: String {
+        switch self {
+        case .rock:
+            return "🪨"
+        case .paper:
+            return "📄"
+        case .scissor:
+            return "✂️"
+        }
+    }
+}
