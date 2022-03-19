b2ebaba377cf59041fa3e34fb8415e32553fb6bc
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-19 16:59:50 -0300

    :wrench: Chore: create simple app project initial configuration

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Gabriel Pereira on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
